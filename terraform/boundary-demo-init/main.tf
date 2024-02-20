c4c85826d915e6b7568fec65150b0f2332017687
Steven Whaley <steven.whaley@hashicorp.com>
2024-02-20 09:59:27 -0800

    Working on adding Vault server

--- terraform/boundary-demo-init/main.tf
+++ terraform/boundary-demo-init/main.tf
@@ -1,42 +1,101 @@
-resource "random_pet" "pet_name" {
+# resource "random_pet" "pet_name" {
+# }
+
+# resource "random_pet" "random_password" {
+#   length = 2
+# }
+
+# #Create New HCP Project for these resources
+# resource "hcp_project" "project" {
+#   name        = "instruqt-${random_pet.pet_name.id}"
+#   description = "Project Created by Instruqt Boundary Demo Lab"
+# }
+
+# #Create HCP Boundary Cluster
+# resource "hcp_boundary_cluster" "boundary-demo" {
+#   project_id = hcp_project.project.resource_id
+#   cluster_id = "instruqt-${random_pet.pet_name.id}"
+#   username   = "admin"
+#   password   = random_pet.random_password.id
+#   tier       = "PLUS"
+# }
+
+resource "random_string" "vault_pass" {
+  length  = 12
+  special = false
 }
 
-resource "random_pet" "random_password" {
-  length = 2
+#Create AWS Public key pair
+resource "aws_key_pair" "vault_key" {
+  key_name = "vault-key"
+  public_key = var.public_key
 }
 
-#Create New HCP Project for these resources
-resource "hcp_project" "project" {
-  name        = "instruqt-${random_pet.pet_name.id}"
-  description = "Project Created by Instruqt Boundary Demo Lab"
+#Create VPC and subnets for EC2 instances
+module "boundary-demo-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = "boundary-demo-vpc"
+  cidr = "10.1.0.0/16"
+
+  azs  = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets  = ["10.1.11.0/24", "10.1.12.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+  enable_dns_hostnames = true
 }
 
-#Create HCP Boundary Cluster
-resource "hcp_boundary_cluster" "boundary-demo" {
-  project_id = hcp_project.project.resource_id
-  cluster_id = "instruqt-${random_pet.pet_name.id}"
-  username   = "admin"
-  password   = random_pet.random_password.id
-  tier       = "PLUS"
+#Create Security Group for Vault instance
+module "vault-security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  name        = "vault-server-access"
+  description = "Allow connection to Vault API"
+  vpc_id      = module.boundary-demo-vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp"]
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8200
+      to_port     = 8200
+      protocol    = "tcp"
+      description = "Connect to Vault UI/API"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      description = "Allow egress to everything within VPC"
+      cidr_blocks = module.boundary-demo-vpc.vpc_cidr_block
+    }
+  ]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["https-443-tcp", "http-80-tcp"]
 }
 
-resource "hcp_hvn" "boundary-vault-hvn" {
-  project_id = hcp_project.project.resource_id
-  hvn_id         = "boundary-vault-demo-hvn"
-  cloud_provider = "aws"
-  region         = var.region
-  cidr_block     = "172.29.16.0/20"
-}
 
-resource "hcp_vault_cluster" "boundary-vault-cluster" {
-  project_id = hcp_project.project.resource_id
-  cluster_id      = hcp_boundary_cluster.boundary-demo.cluster_id
-  hvn_id          = hcp_hvn.boundary-vault-hvn.hvn_id
-  tier            = "dev"
-  public_endpoint = true
-}
+#Create Vault server EC2 instance with AWS Linux AMI
+resource "aws_instance" "vault-server" {
+  ami           = data.aws_ami.vault_ami.id
+  instance_type = "t3.micro"
 
-resource "hcp_vault_cluster_admin_token" "boundary-token" {
-  project_id = hcp_project.project.resource_id
-  cluster_id = hcp_vault_cluster.boundary-vault-cluster.cluster_id
+  key_name                    = aws_key_pair.vault_key.key_name
+  monitoring                  = true
+  subnet_id                   = module.vault-demo-vpc.public_subnets[0]
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [module.vault-security-group.security_group_id]
+  user_data_base64            = base64encode(data.template_file.vault-init.rendered)
+
+  tags = {
+    Name = "vault-demo"
+  }
 }
--- terraform/boundary-demo-init/variables.tf
+++ terraform/boundary-demo-init/variables.tf
@@ -8,3 +8,8 @@ variable "region" {
   default = "us-west-2"
   description = "The AWS region into which to deploy the HVN"
 }
+
+variable "public_key" {
+  type = string
+  description = "Public key to log into AWS instance"
+}
