e739bbfe183d901544d6986cc8c69418a138c738
Steven Whaley <steven.whaley@hashicorp.com>
2024-05-21 15:44:08 -0700

    fix cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,11 +3,15 @@ set -euxo pipefail
 
 source ~/.${INSTRUQT_PARTICIPANT_ID}-env.sh >/dev/null 2>&1
 
-if [[ "$TF_VAR_use_okta" == "true"]]; then
+if [ "$TF_VAR_use_ldap" == "true" ]; then
+    cd ${TF_BASE}/boundary-demo-ad-secrets
+    terraform destroy -auto-approve
+fi
+
+if [ "$TF_VAR_use_okta" == "true" ]; then
     cd ${TF_BASE}/boundary-demo-okta
     terraform destroy -auto-approve
 fi
 
 cd ${TF_BASE}/boundary-demo-init
-terraform destroy -target hcp_boundary_cluster.boundary-demo -auto-approve
-terraform destroy -target hcp_project.project -auto-approve
+terraform destroy -auto-approve
